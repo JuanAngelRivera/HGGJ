8f09158e538154010fb6c74086794e65315e6758
JuanAngelRivera <cherry0jeva@gmail.com>
2025-09-22 13:20:42 -0600

    [Add]Clase para objetos interactuables, player config

--- juego-sin-nombre-xd/scripts/player.gd
+++ juego-sin-nombre-xd/scripts/player.gd
@@ -1,5 +1,6 @@
 extends RigidBody3D
 
+@export var velocity = 1500
 func _process(delta: float) -> void:
 	var input = Vector3.ZERO
 	# Ejes de movimiento con InputMap (WASD configurado antes en Project Settings)
@@ -7,4 +8,4 @@ func _process(delta: float) -> void:
 	input.z = Input.get_axis("move_forward", "move_back")
 	
 	# Aplica la fuerza hacia la dirección de input
-	apply_central_force(input * 1200.0 * delta)
+	apply_central_force(input * velocity * delta)
